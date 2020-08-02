135112f49f1786adc6e124ea8ad93c5677724771
Mukhamed Talaspaev <7636muha@gmail.com>
2020-08-02 23:25:44 +0300

    created dev and prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,3 @@
-const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-module.exports = {
-    entry: {
-        context: path.join(__dirname, 'src'),
-        entry: './src/index.js',
-    },
-    output: {
-        filename: '[Name].js',
-        path: path.join(__dirname, 'dist'),
-        chunkFilename: '[id].[chunkhash].js'
-    },
-    resolve: {
-        extensions: ['.js']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, "src", "index.html")
-        })
-      ],
-      module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
-          }
-        ]
-      },
-    
-    watch: false
-};
+module.exports = process.env.NODE_ENV === 'development'
+    ? require('./webpack.config.dev')
+    : require('./webpack.config.prod');
