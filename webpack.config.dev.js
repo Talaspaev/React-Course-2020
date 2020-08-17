05dc6cc6baa83071a74a3e349b933740c1433e3f
Mukhamed Talaspaev <7636muha@gmail.com>
2020-08-17 10:36:44 +0300

    change for css

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,51 +4,56 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist',
-    },
-    entry: './src/index.js',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js','jsx']
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "index.html")
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                  { loader: MiniCssExtractPlugin.loader },
-                  'css-loader',
-                  'sass-loader'
-                ],
-              },
-            {
-                test: /\.(ttf|eot|svg|woff|png)$/,
-               loader: "file-loader",
-                 options: {
-                 name: '[path][name].[ext]?[hash]'}
-            },
-                
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', 'jsx']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
             }
-        ]
-    },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|svg|woff|png)$/,
+        loader: "file-loader",
+        options: {
+          name: '[path][name].[ext]?[hash]'
+        }
+      },
 
-    watch: true
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+
+  watch: true
 };
